e27e6db82e2fbde7bd839584e32d15ec8c767021
luisangelmaciel <89158021+luisangelmaciel@users.noreply.github.com>
2021-08-18 14:27:45 -0500

    Add files via upload

---
+++ mapsite.js
@@ -0,0 +1,188 @@
+var myObject = {
+    "templetes": [{
+        "title": "Start Bootstrap Theme Modal",
+        "pageid": "/templetes/modal-bootstrap/index.html",
+    }, {
+        "title": "Modal Simple",
+        "pageid": "/templetes/modal-bootstrap/modal/modal-simple.html",
+    }, {
+        "title": "Favorite Modal <i>100% Lighthouse </i><b> <)!</b>",
+        "pageid": "/templetes/modal-bootstrap/favorite/favorite-modal.html",
+    }, {
+        "title": "Modal Text <i>100% Lighthouse</i><b>!</b> ",
+        "pageid": "/templetes/modal-bootstrap/text/modal-text",
+    }, {
+        "title": "Radial Menu <i>100% Lighthouse </i><b> <)!</b>",
+        "pageid": "/templetes/top-nav-radial/index.html",
+    }, {
+        "title": "Radial Menu Inferior",
+        "pageid": "/templetes/radial-menu-inferior/index.html",
+    }, {
+        "title": "Radial Pro",
+        "pageid": "/templetes/radial-pro/radial-pro.html",
+    }, {
+        "title": "Catalogo - multicolor",
+        "pageid": "/templetes/catalogo.html",
+    }, {
+        "title": "Modal Full",
+        "pageid": "/templetes/modal-bootstrap/full/full.html",
+    }, {
+        "title": "404",
+        "pageid": "/templetes/404.html",
+    }, {
+        "title": "Theme type app",
+        "pageid": "/templetes/typeapp/index.html",
+    }, {
+        "title": "Estilo tipo app",
+        "pageid": "/templetes/typeapp/estiloapp.html.html",
+    }, {
+        "title": "Contact card",
+        "pageid": "/templetes/card/contact.html",
+    }, {
+        "title": "Starter",
+        "pageid": "/templetes/starter/index.html",
+    }, {
+        "title": "Full Width Vertical Gallery + Content with Toggle Description",
+        "pageid": "/templetes/bloques/detexto.html.html",
+    }, {
+        "title": "Bootstrap examples",
+        "pageid": "/templetes/bootstrap-examples/",
+    }, {
+        "title": "Profile",
+        "pageid": "/templetes/profile.html",
+    }, {
+        "title": "Templete Blog",
+        "pageid": "/templetes/templetes/tipo/blog.html ",
+    }, {
+        "title": "",
+        "pageid": "/templetes/.html",
+    }, {
+        "title": "",
+        "pageid": "/templetes/.html",
+    }],
+    "howTo": [{
+        "title": "Header ico en movimiento ",
+        "pageid": "/how-to/header-ico-en-movimiento.html",
+    }, {
+        "title": "Simple Tab",
+        "pageid": "/how-to/simple-tab.html",
+    }, {
+        "title": "Tab CSS",
+        "pageid": "/how-to/tab-only-css.html",
+    }, {
+        "title": "Tooltip",
+        "pageid": "/how-to/tooltip.html",
+    }, {
+        "title": "Snackbar",
+        "pageid": "/how-to/snackbar.html",
+    }, {
+        "title": "Alertas",
+        "pageid": "/how-to/alertas.html",
+    }, {
+        "title": "Cursor en movimiento - hand pointer",
+        "pageid": "/how-to/cursor.html",
+    }, {
+        "title": "Open Modal",
+        "pageid": "/how-to/openModal.html.html",
+    }, {
+        "title": "Scroll + header sticky",
+        "pageid": "/how-to/scroll+header.html",
+    }, {
+        "title": "Compare",
+        "pageid": "/how-to/compare.html",
+    }, {
+        "title": "Tab in tab",
+        "pageid": "/how-to/tab-in-tab.html",
+    }, {
+        "title": "Icon sex",
+        "pageid": "/how-to/iconsex.html",
+    }, {
+        "title": "Contentscroll + notas + tooltip + tabs + grind photo + top up",
+        "pageid": "/how-to/contentscroll+notas+tooltip+tabs+grindphoto+toparriba.html",
+    }, {
+        "title": "Change Name",
+        "pageid": "/how-to/change/name.html",
+    }, {
+        "title": "Intro en negro y con movimiento",
+        "pageid": "/how-to/intro-con-movimiento.html",
+    }, {
+        "title": "Simple User Profile Widget",
+        "pageid": "/how-to/simple-User-Profile-Widget.html",
+    }, {
+        "title": "",
+        "pageid": "/how-to/.html",
+    }],
+    "proyectos": [{
+        "proyecto": "Geus Gourmé ",
+        "pageid": "/geus/index.html",
+    }, {
+        "proyecto": "SiOkey",
+        "pageid": "/siokey/index.html",
+    }, {
+        "proyecto": "Tamales",
+        "pageid": "/tamales/",
+    }, {
+        "proyecto": "Mom´s Pizza",
+        "pageid": "/moms-pizza/",
+    }, {
+        "proyecto": "Tamales",
+        "pageid": "/",
+    }, {
+        "proyecto": "Depravex",
+        "pageid": "/depravex/",
+    }, {
+        "proyecto": "",
+        "pageid": "/",
+    }],
+    "luisangelmaciel": [{
+        "title": "Color Mode",
+        "pageid": "/luisangel.maciel/originales/mode/color.html",
+    }, {
+        "title": "Multicolor in tab Mode ",
+        "pageid": "/luisangel.maciel/originales/mode/multicolor.html",
+    }, {
+        "title": "Mode Arcoiris",
+        "pageid": "/luisangel.maciel/originales/mode/modemulticolor.html",
+    }, {
+        "title": "Head user",
+        "pageid": "/luisangel.maciel/user/head.html",
+    }, {
+        "title": "Xiiboster",
+        "pageid": "/luisangel.maciel/originales/xiiboster/",
+    }, {
+        "title": "UI",
+        "pageid": "/luisangel.maciel/originales/ui.html",
+    }, {
+        "title": "go Event!",
+        "pageid": "/luisangel.maciel/originales/go-event",
+    }, {
+        "title": "Infinity history",
+        "pageid": "/luisangel.maciel/originales/infinity-history/index.html",
+    }, {
+        "title": "Colapsable",
+        "pageid": "luisangel.maciel/originales/colapsable/colapsable.html",
+    }, {
+        "title": "Video-chat-v1",
+        "pageid": "/luisangel.maciel/originales/video-chat-v1",
+    }, {
+        "title": "Snackstore",
+        "pageid": "/luisangel.maciel/originales/snackstore",
+    }, {
+        "title": "Pride",
+        "pageid": "/luisangel.maciel/originales/pride.html",
+    }, {
+        "title": "Libro",
+        "pageid": "/luisangel.maciel/book/",
+    }],
+    "notas": [{
+        "proyecto": "Imagen recomendada 749 x 368 px",
+    }, {
+        "proyecto": "Style Guide. <) eBook | Martech para los nuevos. Explicado con bolitas y palitos ",
+    }, {
+        "proyecto": "",
+    }],
+};
+
+// Breaking Bad - Walter White Blue Meth 
+
+w3.displayObject("siteMap", myObject);
