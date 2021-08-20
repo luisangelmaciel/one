7761501bdb4aafec4ca0d354d3833289999c6578
luisangelmaciel <89158021+luisangelmaciel@users.noreply.github.com>
2021-08-19 22:43:40 -0500

    Add files via upload

---
+++ encabezados-titulares/script.js
@@ -0,0 +1,5 @@
+// Experimenting with emoji as image replacements in call-to-action designs
+// Availability of gender modifiers and some newer emoji varies by system
+
+// Some gradients from https://webgradients.com
+// SVG pattern from http://heropatterns.com
