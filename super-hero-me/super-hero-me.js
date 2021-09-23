07f540fb2250634e24ac1530a80c755cdf51a040
Luis Ángel Maciel <89158021+luisangelmaciel@users.noreply.github.com>
2021-09-23 16:55:43 -0500

    Add files via upload

---
+++ super-hero-me/super-hero-me.js
@@ -0,0 +1,6 @@
+const showcase = document.querySelector('.showcase-headerOne');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+})
