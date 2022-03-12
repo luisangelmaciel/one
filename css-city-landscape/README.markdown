5ce9edc7c8b1caae532137da291b534c6eacf50c
Xiiber <hihellomonster@gmail.com>
2022-03-12 10:36:38 -0600

    new

---
+++ css-city-landscape/README.markdown
@@ -0,0 +1,5 @@
+# CSS City Landscape
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/helloluis/pen/NWREdLO](https://codepen.io/helloluis/pen/NWREdLO).
+
+
---
+++ preloader/preloader.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function() {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Portfolio filter
+        --------------------*/
+        $('.portfolio__filter li').on('click', function() {
+            $('.portfolio__filter li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.portfolio__gallery').length > 0) {
+            var containerEl = document.querySelector('.portfolio__gallery');
+            var mixer = mixitup(containerEl);
+        }
+    });
+
+
+
+
+})(jQuery);
