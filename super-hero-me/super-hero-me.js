c79a4522eda4f50c0ee67bde4fa9625880b6f121
Xiiber <hihellomonster@gmail.com>
2022-03-28 23:32:05 -0600

    one

--- super-hero-me/super-hero-me.js
+++
@@ -1,6 +0,0 @@
-const showcase = document.querySelector('.showcase-headerOne');
-
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active');
-    showcase.classList.toggle('active');
-})
